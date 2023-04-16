913b80b37fb85a61a4bc6ee1a0a8108f71e5dcb5
Mike Kokko <mike.kokko@gmail.com>
2023-04-15 22:47:01 -0400

    added piecewise linear mapping, fixed timing

--- lab2/lab2/lab2.ino
+++ lab2/lab2/lab2.ino
@@ -1,12 +1,12 @@
 #include "ArduinoMotorShieldR3.h"
 #include <SPI.h>
 
-#define ARRAY_SIZE 2500   // empirical max = 7500 with 3 arrays(unsigned long, long, and float)
-#define TS 10000          // [us] sampling period
-#define RECORD_TIME 4e6   // [us] duration to record data
+#define ARRAY_SIZE 2500  // empirical max = 7500 with 3 arrays(unsigned long, long, and float)
+#define TS 10000         // [us] sampling period
+#define RECORD_TIME 8e6  // [us] duration to record data
 #define PWM_MAX 400
-#define REF_INPUT 80.0F   // [rad/s]
-#define KP -0.10F         // proportional gain
+#define REF_INPUT 80.0F  // [rad/s]
+#define KP -0.10F        // proportional gain
 
 // encoder chip select pin
 int chipSelectPin1 = 10;
@@ -39,22 +39,23 @@ void loop() {  // treating loop() like main() here, and letting it repeat... our
 
   // array variables
   // TODO: this is getting ridiculous... use a 2D array
-  unsigned long   arr_tim[ARRAY_SIZE]   = {};
-  long            arr_pos[ARRAY_SIZE]   = {};
-  float           arr_vel[ARRAY_SIZE]   = {};
-  float           arr_err[ARRAY_SIZE]   = {};
-  float           arr_vlt[ARRAY_SIZE]   = {};
-  int16_t         arr_pwm[ARRAY_SIZE]   = {};
-  unsigned long   *ptr_tim              = nullptr;
-  long            *ptr_pos              = nullptr;
-  float           *ptr_vel              = nullptr;
-  float           *ptr_err              = nullptr;
-  float           *ptr_vlt              = nullptr;
-  int16_t         *ptr_pwm              = nullptr;  
-  size_t          array_size            = 0;
+  unsigned long arr_tim[ARRAY_SIZE] = {};
+  long arr_pos[ARRAY_SIZE] = {};
+  float arr_vel[ARRAY_SIZE] = {};
+  float arr_err[ARRAY_SIZE] = {};
+  float arr_vlt[ARRAY_SIZE] = {};
+  int16_t arr_pwm[ARRAY_SIZE] = {};
+  unsigned long *ptr_tim = nullptr;
+  long *ptr_pos = nullptr;
+  float *ptr_vel = nullptr;
+  float *ptr_err = nullptr;
+  float *ptr_vlt = nullptr;
+  int16_t *ptr_pwm = nullptr;
+  size_t array_size = 0;
+
 
   // other variables
-  unsigned long progstart, loopstart, curtime, dt;
+  unsigned long progstart, prevloopstart, curtime, dt;
   long encoder3Value, dcount;
   bool array_overrun_flag = false;
   bool TS_overrun_flag = false;
@@ -71,88 +72,92 @@ void loop() {  // treating loop() like main() here, and letting it repeat... our
   ptr_vel = arr_vel;
   ptr_vlt = arr_vlt;
   ptr_err = arr_err;
-  ptr_pwm = arr_pwm;  
+  ptr_pwm = arr_pwm;
   array_size = 0;
 
   // collect data for two seconds
   curtime = micros();
   progstart = curtime;
+  prevloopstart = curtime;
   while (curtime < (progstart + RECORD_TIME)) {
 
-    // TIME CRITICAL OPERATIONS
-
-    // read encoder
-    encoder3Value = getEncoderValue(3);  // as close to micros() as possible
-
-    // compute velocity
-    if (ptr_tim == arr_tim) {
-      *ptr_vel = 0;  // report no speed on first iteration
-    } else {
-      dcount = encoder3Value - *(ptr_pos - 1);
-      dt = (curtime - progstart) - *(ptr_tim - 1);
-      *ptr_vel = (dcount * 1.0F / dt) * ((float)1e6) * (6.28F / 1440.0F);  // simple first backward difference approximation to derivative
-    }
-
-    // compute error signal
-    *ptr_err = ((float)REF_INPUT) - *ptr_vel;
-
-    // compute pwm value
-    *ptr_vlt = ((float)KP)*(*ptr_err);
-    *ptr_pwm = (int16_t)(((float)(*ptr_vlt))*(50.0F));
-
-    // saturate pwm value at limits
-    if((*ptr_pwm) > PWM_MAX){
-      *ptr_pwm = PWM_MAX;
-    } else if((*ptr_pwm) < -1*PWM_MAX) {
-      *ptr_pwm = -1*PWM_MAX;  
-    }
-
-    // write pwm value to motor
-    md.setM1Speed(*ptr_pwm);
-
-    // END OF TIME-CRITICAL OPERATIONS
-
-    // store time and position
-    *ptr_tim = curtime - progstart;  // time stored is [us] since this sampling period began
-    *ptr_pos = encoder3Value;
-
-    // increment pointers and array size
-    ++ptr_tim;
-    ++ptr_pos;
-    ++ptr_vel;
-    ++ptr_err;
-    ++ptr_vlt;
-    ++ptr_pwm;
-    ++array_size;
-
-    // save time that loop started
-    loopstart = curtime;  // absolute count of when this sampling period started
-
-    // check for array overrun
-    // and reset pointers to avoid
-    // actually overrunning allocations
-    if (array_size >= ARRAY_SIZE) {
-      array_size = 0;
-      ptr_tim = arr_tim;
-      ptr_pos = arr_pos;
-      ptr_vel = arr_vel;
-      ptr_err = arr_err;
-      ptr_vlt = arr_vlt;
-      ptr_pwm = arr_pwm;
-      array_overrun_flag = true;
-      Serial.println("************ ARRAY OVERRUN ************");
-    }
-
-    // check whether we overran sample time
-    curtime = micros();
-    if ((curtime - loopstart) > TS) {
-      TS_overrun_flag = true;
-    }
-
     // enforce loop timing
+    // end ensure that we capture a sample right away
+    // first sample or two may be very slightly longer duration
     curtime = micros();
-    while ((curtime - loopstart) < TS) {
+    if (((curtime - prevloopstart) >= TS) || !array_size) {
+
+      // TIME CRITICAL OPERATIONS
+
+      // read encoder
+      encoder3Value = getEncoderValue(3);  // as close to micros() as possible
+
+      // compute velocity
+      if (ptr_tim == arr_tim) {
+        *ptr_vel = 0;  // report no speed on first iteration
+      } else {
+        dcount = encoder3Value - *(ptr_pos - 1);
+        dt = (curtime - progstart) - *(ptr_tim - 1);
+        *ptr_vel = (dcount * 1.0F / dt) * ((float)1e6) * (6.28F / 1440.0F);  // simple first backward difference approximation to derivative
+      }
+
+      // compute error signal
+      *ptr_err = ((float)REF_INPUT) - *ptr_vel;
+
+      // compute control signal: VOLTAGE out of controller
+      *ptr_vlt = ((float)KP) * (*ptr_err);
+
+      // compute control signal: PWM into amplifier
+      *ptr_pwm = v_to_pwm(*ptr_vlt);
+
+      // saturate pwm value at limits
+      if ((*ptr_pwm) > PWM_MAX) {
+        *ptr_pwm = PWM_MAX;
+      } else if ((*ptr_pwm) < -1 * PWM_MAX) {
+        *ptr_pwm = -1 * PWM_MAX;
+      }
+
+      // write pwm value to motor
+      md.setM1Speed(*ptr_pwm);
+
+      // END OF TIME-CRITICAL OPERATIONS
+
+      // store time and position
+      *ptr_tim = curtime - progstart;  // time in [us] since data collection began
+      *ptr_pos = encoder3Value;
+
+      // increment pointers and array size
+      ++ptr_tim;
+      ++ptr_pos;
+      ++ptr_vel;
+      ++ptr_err;
+      ++ptr_vlt;
+      ++ptr_pwm;
+      ++array_size;
+
+      // save time that loop started
+      prevloopstart = curtime;  // absolute count of when this sampling period started
+
+      // check for array overrun
+      // and reset pointers to avoid
+      // actually overrunning allocations
+      if (array_size >= ARRAY_SIZE) {
+        array_size = 0;
+        ptr_tim = arr_tim;
+        ptr_pos = arr_pos;
+        ptr_vel = arr_vel;
+        ptr_err = arr_err;
+        ptr_vlt = arr_vlt;
+        ptr_pwm = arr_pwm;
+        array_overrun_flag = true;
+        Serial.println("************ ARRAY OVERRUN ************");
+      }
+
+      // check whether we overran sample time
       curtime = micros();
+      if ((curtime - prevloopstart) > TS) {
+        TS_overrun_flag = true;
+      }
     }
   }
 
@@ -172,11 +177,11 @@ void loop() {  // treating loop() like main() here, and letting it repeat... our
     Serial.print(*ptr_pos);
     Serial.print(",");
     Serial.print(*ptr_vel, 8);
-        Serial.print(",");
+    Serial.print(",");
     Serial.print(*ptr_err, 8);
-        Serial.print(",");
+    Serial.print(",");
     Serial.print(*ptr_vlt, 8);
-        Serial.print(",");
+    Serial.print(",");
     Serial.println(*ptr_pwm);
     ++ptr_tim;
     ++ptr_pos;
@@ -195,6 +200,36 @@ void loop() {  // treating loop() like main() here, and letting it repeat... our
   delay(3000);
 }
 
+/*
+int16_t v_to_pwm(float v) {
+  return (int16_t)(((float)(v)) * (50.0F));
+}
+/**/
+
+/**/
+int16_t v_to_pwm(float v) {
+
+  int16_t map_pwm[] = { -400, -350, -300, -250, -200, -150, -100, -75, -50, -25, 25, 50, 75, 100, 150, 200, 250, 300, 350, 400 };
+  float map_vlt[] = { -8.23, -8.03, -7.81, -7.53, -7.12, -6.57, -5.58, -4.67, -2.98, -0.23, 0.18, 3.14, 4.80, 5.69, 6.60, 7.18, 7.53, 7.82, 8.05, 8.25 };
+  uint16_t map_length = 20;
+  float frac;
+
+  if (v <= map_vlt[0])
+    return map_pwm[0];
+
+  if (v >= map_vlt[map_length - 1])
+    return map_pwm[map_length - 1];
+
+  for (uint16_t map_idx = 0; map_idx < (map_length - 1); ++map_idx) {
+    if ((v >= map_vlt[map_idx]) && (v <= map_vlt[map_idx + 1])) {
+      frac = (v - map_vlt[map_idx]) / (map_vlt[map_idx + 1] - map_vlt[map_idx]);
+      return (int16_t)(map_pwm[map_idx] + frac * ((float)(map_pwm[map_idx + 1] - map_pwm[map_idx])));
+    }
+  }
+
+  return 0;
+}
+/**/
 
 //*****************************************************
 long getEncoderValue(int encoder)
@@ -219,7 +254,7 @@ long getEncoderValue(int encoder)
 }  //end func
 
 //*************************************************
-void  selectEncoder(int encoder)
+void selectEncoder(int encoder)
 //*************************************************
 {
   switch (encoder) {
--- lab2/matlab/display_data.m
+++ lab2/matlab/display_data.m
@@ -3,10 +3,19 @@ close all; clear; clc;
 
 tab = readtable('test.csv');
 
-t = tab.Var1;
+t = tab.Var1/1e6;
 theta = tab.Var2;
 omega = tab.Var3;
+err = tab.Var4;
+V = tab.Var5;
+pwm = tab.Var6;
 
 figure;
+ax(1) = subplot(2,1,1);
 hold on; grid on
 plot(t,omega);
+ax(2) = subplot(2,1,2);
+hold on; grid on;
+plot(t,pwm);
+
+linkaxes(ax,'x');
